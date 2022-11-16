f6cc7e27ea326fc8f79cb7cc0ff9de8067c2100b
multichiki22 <myguelangel12@gmail.com>
2022-11-16 13:52:51 -0500

    Homework 08 Merge Sort completed Fully tested

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -23,6 +23,22 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if (array.length <= 1) {
+    return array;
+  }
+  let mitad;
+  if (array.length % 2 == 0) mitad = array.length / 2;
+  else mitad = (array.length - 1) / 2;
+  let arr1 = mergeSort(array.slice(0, mitad));
+  let arr2 = mergeSort(array.slice(mitad));
+  let salida = [];
+  while (arr1.length > 0 && arr2.length > 0) {
+    if (arr1[0] < arr2[0]) salida.push(arr1.shift());
+    else salida.push(arr2.shift());
+  }
+  salida = salida.concat(arr1);
+  salida = salida.concat(arr2);
+  return salida;
 }
 
 // No modificar nada debajo de esta línea
